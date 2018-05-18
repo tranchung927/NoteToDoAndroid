a31b25ef4fd9c581d88b98b638577027080ca60b
Chung-sama <tranchung927@gmail.com>
2018-05-18 10:51:57 +0700

    init

---
+++ app/src/main/java/com/example/dung/notetodoandroid/AllTasksActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dung.notetodoandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AllTasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_tasks)
+    }
+}
