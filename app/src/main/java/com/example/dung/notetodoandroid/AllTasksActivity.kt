5af808e451a5f5b3e4656b394d522fa773255b1e
ChungTV <tranchung927@gmail.com>
2018-05-19 15:14:52 +0700

    add TaskDescription

--- app/src/main/java/com/example/dung/notetodoandroid/AllTasksActivity.kt
+++ app/src/main/java/com/example/dung/notetodoandroid/AllTasksActivity.kt
@@ -1,27 +1,34 @@
 package com.example.dung.notetodoandroid
 
-import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_all_tasks.*
-import kotlinx.android.synthetic.main.cell.view.*
 
 class AllTasksActivity : AppCompatActivity(), RecyclerAdapterInterface {
+
+    private var numbers = IntArray(100, { i -> i + 1 }).toMutableList()
+
+    companion object {
+        val PASSING_NUMBER = "passing number"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_tasks)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = RecyclerAdapter(applicationContext, IntArray(100, { i -> i + 1 }).toMutableList(), this)
+        recyclerView.adapter = RecyclerAdapter(applicationContext, numbers, this)
     }
 
     override fun didSelected(item: View, position: Int) {
-        println(position)
+        val intent = Intent(this, TaskDescriptionActivity::class.java).apply {
+            putExtra(PASSING_NUMBER, numbers[position].toString())
+        }
+        startActivity(intent)
     }
+
+
 }
---
+++ app/src/main/java/com/example/dung/notetodoandroid/TaskDescriptionActivity.kt
@@ -0,0 +1,16 @@
+package com.example.dung.notetodoandroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_task_description.*
+
+class TaskDescriptionActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_description)
+        val message = intent.getStringExtra(AllTasksActivity.PASSING_NUMBER)
+        message.let {
+            nameTextView.text = it
+        }
+    }
+}
