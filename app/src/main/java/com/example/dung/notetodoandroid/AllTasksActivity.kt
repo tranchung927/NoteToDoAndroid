0cfc84e64a1cbc121c39570dc49518b3e821353a
Chung-sama <tranchung927@gmail.com>
2018-05-19 08:40:39 +0700

    update cell item

--- app/src/main/java/com/example/dung/notetodoandroid/AllTasksActivity.kt
+++ app/src/main/java/com/example/dung/notetodoandroid/AllTasksActivity.kt
@@ -1,12 +1,44 @@
 package com.example.dung.notetodoandroid
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_all_tasks.*
+import kotlinx.android.synthetic.main.cell.view.*
 
 class AllTasksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_tasks)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = ListAdapter(applicationContext, arrayListOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"))
+    }
+}
+
+class ListAdapter(var c: Context, var lists: ArrayList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        var view: View = LayoutInflater.from(parent.context).inflate(R.layout.cell, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return lists.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ViewHolder).bindData(lists[position])
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bindData(_list: String) {
+            itemView.tv_animal_type.text = _list
+        }
     }
 }
